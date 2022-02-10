8584171585d0a2862d73e95b66c1cc2cbfa8cb42
Sonika Puvvada <69735739+sonikacp@users.noreply.github.com>
2022-02-10 15:57:40 -0500

    fixing error messages

--- flips.js
+++ flips.js
@@ -1,16 +1,15 @@
 import { coinFlips, countFlips } from "./modules/coin.mjs";
 
 const args = process.argv.slice(2);
+if (args.length == 0) {
+  console.log(coinFlips(1));
+  console.log(countFlips(coinFlips(1)));
+  process.exit(1);
+}
 let extracted;
 for (let i = 0; i < args[0].length; i++) {
   extracted = args[0].slice(9, 11);
 }
-// fix this
-if (args.length == 0) {
-    console.log(coinFlips(1));
-    console.log(countFlips(coinFlips(1)));
-    process.exit(1);
-  }
-  
-console.log(coinFlips(extracted));
-console.log(countFlips(coinFlips(extracted)));
+const coinFlipsNum = coinFlips(extracted);
+console.log(coinFlipsNum);
+console.log(countFlips(coinFlipsNum)); // why is this printing the wrong number???
--- guess-flip.js
+++ guess-flip.js
@@ -1,13 +1,13 @@
 import { flipACoin } from "./modules/coin.mjs";
 const args = process.argv.slice(2);
+if (args.length==0) {
+  console.error('Error: no input.\nUsage: node guess-flip --call=[heads|tails]');
+  process.exit(1);
+}
 let extracted;
 for (let i = 0; i < args[0].length; i++) {
   extracted = args[0].slice(7, 12);
 }
-console.log('args.length:', args.length);
-// fix this
-if (args.length==0) {
-    console.error('Error: no input. \n Usage: node guess-flip --call=[heads|tails]');
-    process.exit(1);
-}
+// console.log('args.length:', args.length);
+
 console.log(flipACoin(extracted));
