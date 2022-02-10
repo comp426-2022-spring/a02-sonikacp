7f9a9a6b7b08ed6cd48fa3f11f337881ffe7fd10
Sonika Puvvada <69735739+sonikacp@users.noreply.github.com>
2022-02-10 15:44:48 -0500

    no argument attempt

--- guess-flip.js
+++ guess-flip.js
@@ -4,8 +4,10 @@ let extracted;
 for (let i = 0; i < args[0].length; i++) {
   extracted = args[0].slice(7, 12);
 }
-if (process.argv.length != 3) {
+console.log('args.length:', args.length);
+// fix this
+if (args.length==0) {
     console.error('Error: no input. \n Usage: node guess-flip --call=[heads|tails]');
     process.exit(1);
-  }
+}
 console.log(flipACoin(extracted));
